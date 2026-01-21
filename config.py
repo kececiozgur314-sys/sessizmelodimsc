fdf9bf18bed43f54035008651cd29dbc3f040b57
kececiozgur314-sys <kececiozgur314@gmail.com>
2026-01-21 17:20:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,19 +12,19 @@ def _env_bool(key: str, default: bool = False) -> bool:
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", 0))
-        self.API_HASH = getenv("API_HASH")
+        # Core
+        self.API_ID = int(getenv("API_ID", "31939892"))
+        self.API_HASH = getenv("API_HASH", "a7f3a115764c8c6eebba8b3cfdccc022")
+        self.BOT_TOKEN = getenv("BOT_TOKEN", ""8441056561:AAFhSeSQ49OoXZiuipYD-J94xjgjISd4KS0"")
+        self.MONGO_URL = getenv("MONGO_URL", "mongodb+srv://mongoguess:guessmongo@cluster0.zcwklzz.mongodb.net/?retryWrites=true&w=majority"") or getenv("MONGO_DB_URI")
+        self.LOGGER_ID = int(getenv("LOGGER_ID", "-1003639948579"))
+        self.OWNER_ID = int(getenv("OWNER_ID", "8237345360"))
+        self.OWNER_USERNAME = getenv("OWNER_USERNAME", "@gebermedim")
+        self.BOT_USERNAME = getenv("BOT_USERNAME", "@FizyMusic_bot")
+        self.BOT_NAME = getenv("BOT_NAME", "Fizy Music")
+        self.ASSUSERNAME = getenv("ASSUSERNAME", "Fizy Assistant")
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN")
-        self.MONGO_URL = getenv("MONGO_URL") or getenv("MONGO_DB_URI")
-
-        self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
-        self.OWNER_ID = int(getenv("OWNER_ID", 0))
-        self.OWNER_USERNAME = getenv("OWNER_USERNAME", "")
-        self.BOT_USERNAME = getenv("BOT_USERNAME", "")
-        self.BOT_NAME = getenv("BOT_NAME", "")
-        self.ASSUSERNAME = getenv("ASSUSERNAME", "")
-
+        # Limits
         self.DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
         self.DURATION_LIMIT = self.DURATION_LIMIT_MIN * 60
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
@@ -35,27 +35,35 @@ class Config:
         self.SONG_DOWNLOAD_DURATION_LIMIT = int(
             getenv("SONG_DOWNLOAD_DURATION_LIMIT", "1800")
         )
-        self.TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "157286400"))
-        self.TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1288490189"))
-
-        self.SESSION1 = (
-            getenv("SESSION1")
-            or getenv("SESSION")
-            or getenv("STRING_SESSION")
-            or None
+        self.TG_AUDIO_FILESIZE_LIMIT = int(
+            getenv("TG_AUDIO_FILESIZE_LIMIT", "157286400")
         )
-        self.SESSION2 = getenv("SESSION2") or getenv("STRING_SESSION2", None)
-        self.SESSION3 = getenv("SESSION3") or getenv("STRING_SESSION3", None)
+        self.TG_VIDEO_FILESIZE_LIMIT = int(
+            getenv("TG_VIDEO_FILESIZE_LIMIT", "1288490189")
+        )
+
+        # Sessions
+        self.SESSION1 = getenv("SESSION1", "AQHnXTQAi68yQjil8KVfamyUqysy7oWGAhPj8qdWheYD6TZl3nkEXWVN0vQZHRiE33loY9u9gky96Lx048seTOQrFn7mDU2QJDf9cgXwCDyvn0BXzNBsWYYKeXW0BsPr8c8r9OoOaaB6pvdjTC_ZB6i-q6iuHNHn45-6LymzC3iiCFdxAbMuJEl6t4B1FowyG0CaoJI2kVAOsnhZK0Qy5eDkIEhHvy0g3HZovE6PlVYjqefzAL0lNY-QwMH6J4c96v26mjS2ewmkcEjsmBxaQql8TcxEIQ6y7-wlgcNu0SMryuHB-pVNVOuAOOa6_fP_QAliftksMACtJANg35gnudhfpVbp5QAAAAH00xWPAA")
+        if not self.SESSION1:
+            self.SESSION1 = getenv("SESSION")
+        if not self.SESSION1:
+            self.SESSION1 = getenv("STRING_SESSION")
+
+        self.SESSION2 = getenv("SESSION2") or getenv("STRING_SESSION2")
+        self.SESSION3 = getenv("SESSION3") or getenv("STRING_SESSION3")
+
         self.STRING4 = getenv("STRING_SESSION4")
         self.STRING5 = getenv("STRING_SESSION5")
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        # Support
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/sadistimki")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/prensesimu")
 
+        # Playback / behavior
         self.AUTO_END = _env_bool("AUTO_END", False)
         self.AUTO_LEAVE = _env_bool("AUTO_LEAVE", False)
         self.VIDEO_PLAY = _env_bool("VIDEO_PLAY", True)
-        cookie_env = getenv("COOKIES_URL", "")
+        cookie_env = getenv("COOKIES_URL", "https://batbin.me/stars")
         if not cookie_env:
             cookie_env = getenv("COOKIE_URL", "")
         self.COOKIES_URL = [
@@ -64,10 +72,14 @@ class Config:
         ]
         if not self.COOKIES_URL and cookie_env:
             self.COOKIES_URL = [cookie_env]
+
+        # External APIs
         self.API_URL = getenv("API_URL")
         self.VIDEO_API_URL = getenv("VIDEO_API_URL")
         self.API_KEY = getenv("API_KEY")
         self.DEEP_API = getenv("DEEP_API")
+
+        # Deployment / Git
         self.HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
         self.HEROKU_API_KEY = getenv("HEROKU_API_KEY")
         self.UPSTREAM_REPO = getenv(
@@ -75,8 +87,13 @@ class Config:
         )
         self.UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
         self.GIT_TOKEN = getenv("GIT_TOKEN")
+
+        # Integrations
         self.SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID")
         self.SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET")
+        self.TMDB_API_KEY = getenv("TMDB_API_KEY", "")
+
+        # Media assets
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
         self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
         self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
@@ -86,6 +103,8 @@ class Config:
         )
         self.THUMB_FONT = getenv("THUMB_FONT", "anony/assets/thumb/font.ttf")
         self.THUMB_FONT2 = getenv("THUMB_FONT2", "anony/assets/thumb/font2.ttf")
+
+        # Stream quality
         self.AUDIO_QUALITY = getenv("AUDIO_QUALITY", "HIGH")
         self.VIDEO_QUALITY = getenv("VIDEO_QUALITY", "HD_720p")
         self.STREAM_CACHE = int(getenv("STREAM_CACHE", 200))
@@ -94,6 +113,8 @@ class Config:
             "STREAM_FFMPEG",
             "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
         )
+
+        # Start media
         self.START_VIDS = [
             "https://telegra.ph/file/9b7e1b820c72a14d90be7.mp4",
             "https://telegra.ph/file/72f349b1386d6d9374a38.mp4",
@@ -103,6 +124,8 @@ class Config:
             "CAACAgUAAx0Cld6nKUAACASBI_rna1O1e6g7qS-ry-aZ1ZpVEnwACgg8AAi2LEfFI5wfykOC4h4E",
             "CAACAgUAAx0Cld6nKUAACATJI_rsEJOsaAPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ",
         ]
+
+        # UI assets
         self.HELP_IMG_URL = getenv("HELP_IMG_URL", "https://files.catbox.moe/yg2vky.jpg")
         self.PING_VID_URL = getenv("PING_VID_URL", "https://files.catbox.moe/3ivvgo.mp4")
         self.PLAYLIST_IMG_URL = getenv("PLAYLIST_IMG_URL", "https://files.catbox.moe/yhaja5.jpg")
@@ -115,6 +138,8 @@ class Config:
         self.SPOTIFY_ARTIST_IMG_URL = getenv("SPOTIFY_ARTIST_IMG_URL", self.YOUTUBE_IMG_URL)
         self.SPOTIFY_ALBUM_IMG_URL = getenv("SPOTIFY_ALBUM_IMG_URL", self.YOUTUBE_IMG_URL)
         self.SPOTIFY_PLAYLIST_IMG_URL = getenv("SPOTIFY_PLAYLIST_IMG_URL", self.YOUTUBE_IMG_URL)
+
+        # Emojis
         self.PLAY_EMOJIS = [
             "💞",
             "🦋",
@@ -135,8 +160,10 @@ class Config:
 
     def check(self):
         missing = []
-        required = ["API_ID", "API_HASH", "BOT_TOKEN", "MONGO_URL", "LOGGER_ID", "OWNER_ID"]
+        required = ["API_ID", "API_HASH", "BOT_TOKEN", "LOGGER_ID", "OWNER_ID"]
         missing.extend([var for var in required if not getattr(self, var)])
+        if not (self.MONGO_URL or getenv("MONGO_DB_URI")):
+            missing.append("MONGO_URL")
         if not self.SESSION1:
             missing.append("SESSION1")
         if missing:
